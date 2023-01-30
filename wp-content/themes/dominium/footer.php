64895cb024364ba185ea216bfdaa10e707edf6b2
Rita Kassiane <ritakassiane.t.i@gmail.com>
2023-01-29 21:16:49 -0300

    :sparkles: Add: Footer

--- wp-content/themes/dominium/footer.php
+++ wp-content/themes/dominium/footer.php
@@ -1,3 +1,52 @@
 <?php wp_footer(); ?>
+<footer>
+        <div class="container">
+            <div class="footer-column1">
+                <div class="logo">
+                    <img src="<?php echo theme_image('logo-dominium.svg') ?>" alt="Logo Dominium">
+                    <h2>Colégio Dominium</h2>
+                </div>
+
+                <address>
+                    Rua Prado, 4 - Núcleo Habitacional Teofilândia-BA
+                </address>
+            </div>
+
+            <div class="nav-lateral">
+                <ul>
+                    <li><a href="index.html">Página Inicial</a></li>
+                    <li><a href="sobre-nos.html">Sobre nós</a></li>
+                    <li><a href="ensino.html">Ensino</a></li>
+                    <li><a href="calendario.html">Calendário</a></li>
+                    <li><a href="noticias.html">Notícias</a></li>
+                    <li><a href="galeria">Galeria</a></li>
+                    <li><a href="acervo.html">Acervo</a></li>
+                    <li><a href="livraria.html">Livraria</a></li>
+                    <li><a href="matricula.html">Matrícula</a></li>
+                </ul>
+            </div>
+        </div>
+        
+    </footer>
+    <div class="rodape">
+        <div class="container">
+            <div class="plataformas">
+                <ul>
+                    <li><img src="<?php echo theme_image('logo-pitagoras.svg') ?>" alt="Logo rede Pitágoras"></li>
+                    <li><img src="<?php echo theme_image('logo-escola-da-inteligencia.svg') ?>" alt="Logo Escola da Inteligência"></li>
+                </ul>
+            </div>
+            
+            <div class="links-externos">
+                <ul>
+                    <li><a href="*">Trabalhe Conosco</a></li>
+                    <li><a href="*">Fale Conosco</a></li>
+                    <li><a href="*">Acessibilidade</a></li>
+                    <li><a href="*">Termos e Serviços</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
